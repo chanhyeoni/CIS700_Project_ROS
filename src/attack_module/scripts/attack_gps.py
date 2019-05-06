757d1d0a91b272025322a12c7f7f3975a2bcdeca
chanhyeoni <chan.hyeo.ni.3698@gmail.com>
2019-05-06 03:50:48 +0000

    error fix in attach_gps.py

--- src/attack_module/scripts/attack_gps.py
+++ src/attack_module/scripts/attack_gps.py
@@ -1,37 +1,4 @@
 #!/usr/bin/env python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2019, Chang Hyun Lee
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
 
 '''
 	attack_gps republises manually generated, anomolous data to the same topic 
@@ -59,14 +26,18 @@ anomaly_data_range = 0
 	a callback function that assigns a subcribed GPS data from topic (/vehicle/perfect_gps/enhanced_fix) to normal_data, which will be used to create fake data
 '''
 def get_normal_GPSFix_callback(data):
-	normal_data = data
+	normal_data.latitude = data.latitude
+	normal_data.longitude = data.longitude
+	normal_data.track = data.track
+	normal_data.speed = data.speed
+	normal_data.climb = data.climb
 
 
 ''' 
 	a function that returns a fake GPS data with wrong, anomalous latitude and longitude.
 	In detail, the function takes parameter data suscribed from the topic and
 '''
-def create_anomalous_GPSFix(command, data):
+def create_anomalous_GPSFix(command):
 	# copy 
 	if (command == 'zero_lat_long'):
 		data.latitude = 0
@@ -110,7 +81,7 @@ if __name__ == '__main__':
 		# set the rate
 		rate = rospy.Rate(publish_rate)
 		while not rospy.is_shutdown():
-			attack_data = create_anomalous_GPSFix(sensor_attack_type, normal_data)
+			attack_data = create_anomalous_GPSFix(sensor_attack_type)
 			pub_original.publish(attack_data)
 			rospy.loginfo("anomalous data published!")
 			rate.sleep()
